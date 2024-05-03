6f2714872f5a0e978665bea78c50e5c6f8b508b7
loverikim <loverikim03@gmail.com>
2024-05-03 09:41:32 +0900

    MainClass 추가 0429 java복습

---
+++ src/testProject_0503/MainClass.java
@@ -0,0 +1,11 @@
+package testProject_0503;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("helloworld");
+
+	}
+
+}
